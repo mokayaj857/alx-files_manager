658185efce79439a0b341bb7a83f1b475e9226db
john mokaya <mokayaj857@gmail.com>
2024-12-24 08:09:24 +0300

    Update main_db.js

--- main_db.js
+++ main_db.js
@@ -1,7 +1,7 @@
 import dbClient from './utils/db';
 
 const waitConnection = () => new Promise((resolve, reject) => {
-  let i = 0;
+  let i = 0;,
   const repeatFct = async () => {
     await setTimeout(() => {
       i += 1;
